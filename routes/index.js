c44d75af51974580da4512ebd556da9ccada407d
Habibie Faried <habibiefaried@gmail.com>
2022-03-18 21:24:09 +0400

    feat: add key

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let ak = "AKIAJIPU77TQL5LB5OIB"
+  let as = "8Mw77pe6Ua9wr56f6lr169rDPTDWeUvV0q6ZS+6N"
   res.render('index', { title: 'Express' });
 });
 
